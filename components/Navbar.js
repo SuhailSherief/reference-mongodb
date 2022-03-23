e64c27b5bc0c12a2c3d5e9aab537e53c452261ce
SuhailSherief <suhailscreed@gmail.com>
2022-03-23 23:57:48 +0530

    navbar

---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <Link href="/">
+                <a className="navbar-brand">Notes App</a>
+            </Link>
+            <Link href="/new">
+                <a className="create">Create Note</a>
+            </Link>
+        </nav>
+    )
+}
+
+export default Navbar
